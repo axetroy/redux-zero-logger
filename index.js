7e8cd19b7ff71b9136bba0298032e7bef1e91a2e
axetroy <troy450409405@gmail.com>
2018-01-26 17:38:29 +0800

    chore: build with libpack in painless

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-function logger(config) {
-  config = config || {};
+/**
+ * The logger middleware for redux-zero
+ * @param config
+ * @returns {function(*): function(*): function(*=)}
+ */
+function logger(config = {}) {
+  // return an middleware
+  return state => next => action => {
+    const prevState = store.getState();
+    const r = next(action);
 
-  // return middleware
-  return function(store) {
-    return function(next) {
-      return function(action) {
-        const prevState = store.getState();
-        const r = next(action);
+    function prinf(prev, action, next) {
+      console.group(action, "@" + new Date().toISOString());
+      console.log("%cprev state", "color:#9E9E9E", prev);
+      console.log("%caction", "color:#2196F3", action);
+      console.log("%cnext state", "color:#4CAF50", next);
+      console.groupEnd();
+    }
 
-        function prinf(prev, action, next) {
-          console.group(action, "@" + new Date().toISOString());
-          console.log("%cprev state", "color:#9E9E9E", prev);
-          console.log("%caction", "color:#2196F3", action);
-          console.log("%cnext state", "color:#4CAF50", next);
-          console.groupEnd();
-        }
-
-        if (r && typeof r.then === "function") {
-          return next(action).then(function(d) {
-            prinf(prevState, action.name, store.getState());
-            return Promise.resolve(d);
-          });
-        } else {
-          prinf(prevState, action.name, store.getState());
-          return r;
-        }
-      };
-    };
+    if (r && typeof r.then === "function") {
+      return next(action).then(
+        d =>
+          prinf(prevState, action.name, store.getState()) && Promise.resolve(d)
+      );
+    } else {
+      prinf(prevState, action.name, store.getState());
+      return r;
+    }
   };
 }
 
